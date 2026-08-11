ecdad18d72305a9d1d80af048f455e6f0b1463c1
Mikhail <govgolenkom@icloud.com>
2026-08-11 15:21:04 +0300

    fix: update base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Pet-projects/",
+  base: "/",
   server: { host: "127.0.0.1", port: 4321, open: true },
 });
