5b5a0798f5a77bfc6f83aca9434bb0d3bd8ce980
Mikhail <govgolenkom@icloud.com>
2026-08-26 18:15:47 +0300

    add fun agenda

--- src/pages/cards.tsx
+++ src/pages/cards.tsx
@@ -12,4 +12,14 @@ export const cards = [
     readmeUrl: "https://github.com/MikhailGovgolenko/Pet-projects/tree/main/src/lens#readme",
     component: lazy(() => import("../lens/LensPage")),
   },
+  {
+    id: "agenda",
+    icon: "📋",
+    previewLight: "agenda-preview-light.png",
+    previewDark: "agenda-preview-dark.png",
+    titleKey: "card.agenda.title" as TranslationKey,
+    descKey: "card.agenda.desc" as TranslationKey,
+    readmeUrl: "#",
+    component: lazy(() => import("../agenda/AgendaPage")),
+  },
 ];
